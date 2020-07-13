14664fdf5aafd4479b160df66c810a275ac64cbe
Martin-Minecraft <67168306+Martin-Minecraft@users.noreply.github.com>
2020-07-13 15:27:26 +0200

    Create Skyblox_Hub.lua

---
+++ Skyblox_Hub.lua
@@ -0,0 +1,54 @@
+-- Skyblox_Hub
+
+key is: none
+
+
+local library = loadstring(game:HttpGet("https://pastebin.com/raw/SKZjdjdj"))();
+
+
+local Gamer = library:CreateSection("https://pastebin.com/raw/PswbUqFp");
+
+setclipboard("https://pastebin.com/raw/PswbUqFp")
+
+end)
+
+local Gamer = library:CreateSection("Hub");
+
+Gamer:Button("(GUI)-Skyblox-Hub",function()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Martin-Minecraft/Multi-Friends-Hub-V1/master/(GUI)-Skyblox-Hub.lua"))();
+
+end)
+
+Gamer:Button("only if you use Serverside execute this in Citrus-Gui",function()
+
+setclipboard("https://pastebin.com/raw/PswbUqFp")
+
+end)
+
+local Gamer2 = library:CreateSection("GUI");
+
+Gamer2:Button("Skyblox-(GUI)",function()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Martin-Minecraft/Multi-Friends-Hub-V1/master/(GUI)-Skyblox.lua"))();
+
+end)
+
+Gamer2:Button("Skyblox-(GUI)",function()
+
+-- Cheat.gg redistribute, the best source for Roblox Cheats, Exploits & Scripts!
+
+PastebinBanned = false
+loadstring(game:HttpGet("https://system-exodus.com/scripts/Skyblock/Skyblock.lua",true))()
+
+end)
+
+local Gamer = library:CreateSection("none");
+Gamer:Toggle("idk");
+Gamer:Box("C");
+Gamer:ColorPicker("D");
+Gamer:Dropdown("Bakon", {"A", "B", "C"});
+Gamer1:Button("Gamer Button");
+
+
+library:Ready();
