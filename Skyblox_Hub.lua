5f3570dd16db428c123e5c804223d64634605495
Martin-Minecraft <67168306+Martin-Minecraft@users.noreply.github.com>
2020-07-13 15:43:07 +0200

    Update Skyblox_Hub.lua

--- Skyblox_Hub.lua
+++ Skyblox_Hub.lua
@@ -1,54 +1,39 @@
 -- Skyblox_Hub
 
--- key is: none
-
 
 local library = loadstring(game:HttpGet("https://pastebin.com/raw/SKZjdjdj"))();
 
 
-local Gamer = library:CreateSection("https://pastebin.com/raw/PswbUqFp");
+local Gamer = library:CreateSection("idk");
 
-setclipboard("https://pastebin.com/raw/PswbUqFp")
+Gamer:Button("only if you use Serverside execute this in Citrus-Gui",function()
+
+setclipboard("loadstring(game:HttpGet(('https://pastebin.com/raw/PswbUqFp'),true))()")
 
 end)
 
-local Gamer = library:CreateSection("Hub");
+local Gamer2 = library:CreateSection("Hub");
 
-Gamer:Button("(GUI)-Skyblox-Hub",function()
+Gamer2:Button("(GUI)-Skyblox-Hub",function()
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Martin-Minecraft/Multi-Friends-Hub-V1/master/(GUI)-Skyblox-Hub.lua"))();
 
 end)
 
-Gamer:Button("only if you use Serverside execute this in Citrus-Gui",function()
+local Gamer3 = library:CreateSection("GUI");
 
-setclipboard("https://pastebin.com/raw/PswbUqFp")
-
-end)
-
-local Gamer2 = library:CreateSection("GUI");
-
-Gamer2:Button("Skyblox-(GUI)",function()
+Gamer3:Button("Skyblox-(GUI)-Sky-Block-fucker-V2",function()
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Martin-Minecraft/Multi-Friends-Hub-V1/master/(GUI)-Skyblox.lua"))();
 
 end)
 
-Gamer2:Button("Skyblox-(GUI)",function()
+Gamer3:Button("Skyblox-(GUI)-Finity-UI",function()
 
 -- Cheat.gg redistribute, the best source for Roblox Cheats, Exploits & Scripts!
 
 PastebinBanned = false
 loadstring(game:HttpGet("https://system-exodus.com/scripts/Skyblock/Skyblock.lua",true))()
-
-end)
-
-local Gamer = library:CreateSection("none");
-Gamer:Toggle("idk");
-Gamer:Box("C");
-Gamer:ColorPicker("D");
-Gamer:Dropdown("Bakon", {"A", "B", "C"});
-Gamer1:Button("Gamer Button");
 
 end)
 
